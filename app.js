8d7b4376cb9b2d3b5f29afe7f5aa19d63499e9df
Mark Bishoff <markbish24@gmail.com>
2025-08-06 05:11:55 -0700

    practice async

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-const { readFileSync, writeFileSync } = require("fs");
+const { readFile, writeFile } = require("fs");
 
-const first = readFileSync("./content/first.txt", "utf-8");
-const second = readFileSync("./content/second.txt", "utf-8");
+readFile("./content/first.txt", "utf8", (err, result) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(result);
 
-writeFileSync(
-  "./content/results-sync.txt",
-  `Here is the result ${first}, ${second} `
-);
+  const first = result;
+  readFile("./content/second.txt", "utf8", (err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const second = result;
+    writeFile(
+      "./content/result-async.txt",
+      `Here is the result ${first}, ${second} `,
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log(result);
+      }
+    );
+  });
+});
