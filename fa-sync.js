75e08ee7a991fde0d56bcc4e5ab2d33cf138e609
Mark Bishoff <markbish24@gmail.com>
2025-08-07 12:52:28 -0700

    completed more of a section

--- fa-sync.js
+++ fa-sync.js
@@ -1,4 +1,5 @@
 const { readFileSync, writeFileSync } = require("fs");
+console.log("start");
 
 const first = readFileSync("./content/first.txt", "utf-8");
 const second = readFileSync("./content/second.txt", "utf-8");
@@ -7,3 +8,6 @@ writeFileSync(
   "./content/results-sync.txt",
   `Here is the result ${first}, ${second} `
 );
+
+console.log("done with this task");
+console.log("starting the next one");
