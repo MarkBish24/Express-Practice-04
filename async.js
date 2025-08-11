8da43940c44aa72975bf365e3e0c41ab8f3f64a9
Mark Bishoff <markbish24@gmail.com>
2025-08-11 10:44:55 -0700

    more practice

---
+++ async.js
@@ -0,0 +1,31 @@
+const { readFile, writeFile } = require("fs");
+const util = require("util");
+const readFilePromise = util.promisify(readFile);
+const writeFilePromise = util.promisify(writeFile);
+
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, "utf8", (err, result) => {
+//       if (err) {
+//         reject(err);
+//       }
+//       resolve(result);
+//     });
+//   });
+// };
+
+// getText("./content/first.txt")
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
+
+const start = async () => {
+  try {
+    const first = await readFilePromise("./content/first.txt", "utf8");
+    const second = await readFilePromise("./content/second.txt", "utf8");
+    console.log(first, second);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
